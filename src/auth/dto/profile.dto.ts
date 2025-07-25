59be768e39292a1934159fe24859bf4f480d72af
mohammad afzalzadeh <afzalzademohammad@gmail.com>
2025-07-25 13:49:14 +0330

    add dto for change profile

---
+++ src/auth/dto/profile.dto.ts
@@ -0,0 +1,50 @@
+import {
+  IsDate,
+  IsEmail,
+  IsOptional, 
+  IsString,
+  MaxDate,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+
+
+
+export class ProfileDto{
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ example: 'mohammad', description: 'username' , required: false })
+  username?:string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ example: 'mohammad', description: 'first name' , required: false })
+  firstName?:string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ example: 'afzal', description: 'last name' , required: false })
+  lastName?:string;
+
+  @IsOptional()
+  @IsEmail()
+  @ApiProperty({ example: 'mohammad@gmail.com', description: 'email' , required: false })
+  email?:string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ example: 'data engineer', description: 'job' , required: false })
+  job?:string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ example: 'bio...', description: 'bio' , required: false })
+  bio?:string;
+
+  @IsOptional()
+  @Transform(({ value }) => new Date(value), { toClassOnly: true })
+  @IsDate()
+  @MaxDate(new Date('2007-10'))
+  @ApiProperty({ example: '2001-10-13', description: 'birthday' , required: false })
+  birthday?:Date;
+}
