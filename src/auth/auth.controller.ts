eb0fe3f3b4af61cd30cbafd25a782a1c1ee69f27
mohammad afzalzadeh <afzalzademohammad@gmail.com>
2025-07-27 21:37:11 +0330

    upload and download profile photos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,10 +8,16 @@ import {
   UnauthorizedException,
   UseGuards, Patch,
   BadRequestException,
+  Param,
+  UseInterceptors,
+  UploadedFile
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signUp.dto';
 import { LogInDto } from './dto/logIn.dto';
+import { ProfileImageDto } from './dto/image.dto'
+import { writeFile } from 'fs/promises';
+import { createReadStream } from 'fs'
 import { Response as Res, Request as Req } from 'express';
 import { AuthGuard } from './auth.guard';
 import { env } from 'node:process';
@@ -21,10 +27,19 @@ import { ActiveUserDto } from './dto/activeUser.dto';
 import { ChangeRole } from './dto/changeRole.dto'
 import { AuthEntity, RoleEntity } from './auth.entity';
 import { ProfileDto } from './dto/profile.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import * as path from 'node:path';
+import * as mime from 'mime-types'; 
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService:AuthService) {}
+  constructor(
+    private authService:AuthService,
+    private configService: ConfigService
+  ) {}
 
   @Post('/signUp')
   signUp(@Body() signUpDto:SignUpDto): Promise<AuthEntity[]>{
@@ -89,5 +104,27 @@ export class AuthController {
   async changeProfile(@Request() req:Req, @Body() profileDto:ProfileDto){
     const userId = req['user'].sub
     return this.authService.changeProfile(userId , profileDto)
+  }
+
+  @Get('/image/:userId')
+  getProfileImage(@Param('userId') userId:string , @Response() res:Res){
+    const profileImageBasePath = this.configService.get('PROFILE_IMAGE_BASE_PATH')
+    const image = createReadStream(path.join(profileImageBasePath , userId));
+    image.pipe(res);
+  }
+  
+  @UseGuards(AuthGuard)
+  @Post('/image')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({ 
+    type: ProfileImageDto
+  })
+  @UseInterceptors(FileInterceptor('profileImage'))
+  async updateProfileImage(@Request() req:Req, @UploadedFile() profileImage: Express.Multer.File){
+    const userId = req['user'].sub
+    const profileImageBasePath = this.configService.get('PROFILE_IMAGE_BASE_PATH')
+    await writeFile(path.join(profileImageBasePath, userId ) , profileImage.buffer)
+    return {message : 'file uploaded succesfully.' }
+
   }
 }
